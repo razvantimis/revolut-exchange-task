a419cd04a756bdab56e1e5be14ba3dd69a949c3f
Razvan Timis <timisrazvanvasile@gmail.com>
2021-05-22 16:51:18 +0300

    Add unit test for setValue from and to

--- src/state-management/__tests__/exchangeSlice.test.ts
+++ src/state-management/__tests__/exchangeSlice.test.ts
@@ -2,6 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import exchange, {
   setCurrencyFrom,
   setCurrencyTo,
+  setValueFrom,
+  setValueTo,
   switchCurrency,
 } from '../exchangeSlice';
 import { CurrencyType } from '../enum';
@@ -17,6 +19,7 @@ describe('Store => exchangeSilce', () => {
   };
 
   const getExchangeStore = (store: ReturnType<typeof setupStore>) => store.getState().exchange;
+
   it('should set currencyFrom to USD', () => {
     const store = setupStore();
     store.dispatch(setCurrencyFrom(CurrencyType.USD));
@@ -39,5 +42,21 @@ describe('Store => exchangeSilce', () => {
 
     expect(getExchangeStore(store).currencyTo).toBe(CurrencyType.GPB);
     expect(getExchangeStore(store).currencyFrom).toBe(CurrencyType.EUR);
+  });
+
+  it('should set valueTo  = 12.05', () => {
+    const store = setupStore();
+    const nextValue = "12.05"
+    store.dispatch(setValueTo(nextValue));
+
+    expect(getExchangeStore(store).valueTo).toBe(nextValue);
+  });
+
+  it('should set valueFrom  = 156.05', () => {
+    const store = setupStore();
+    const nextValue = "156.05"
+    store.dispatch(setValueFrom(nextValue));
+
+    expect(getExchangeStore(store).valueFrom).toBe(nextValue);
   });
 });
